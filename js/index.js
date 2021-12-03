c7e33b40a4af75730ed442efd3630d9540e1e17e
Tywayne <tywaynebuchanan@gmail.com>
2021-12-03 15:57:45 -0500

    made site mobile ready

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
 const skill_wrap = document.querySelector(".skills");
 const skill_bars = document.querySelectorAll(".skill-progress");
-
 const filter_btns = document.querySelectorAll('.filter-btn');
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
 
+function closeMenu()
+{
+    navbar.classList.remove("open");
+    document.body.classList.remove("stop-scrolling");
+}
+hamburger_menu.addEventListener("click", ()=>{
+    if(!navbar.classList.contains("open"))
+    {
+        document.body.classList.add("stop-scrolling");
+        navbar.classList.add("open");
+    }else{
+        closeMenu();
+    }
+})
+
+links.forEach((link) => link.addEventListener("click", ()=>closeMenu()))
 
 filter_btns.forEach(btn => btn.addEventListener('click', ()=>{
     filter_btns.forEach(button => button.classList.remove("active"));
